aa7cb4d8e65dbb3996992f14bcb42b63a5bbc856
Tristan Chambers <tristan.chambers@gmail.com>
2017-01-23 21:48:26 -0500

    Use proper 404 function

--- petition/views.py
+++ petition/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 # https://docs.djangoproject.com/en/1.10/topics/http/views/
@@ -11,6 +12,6 @@ def petition_list(request):
     return render(request, 'petition/petition_list.html', {'petitions': petitions})
 
 def petition_detail(request, primary_key):
-    petition = Petition.objects.get(pk=primary_key)
+    petition = get_object_or_404(Petition, pk=primary_key)
     signatures = Signature.objects.filter(petition=1)
     return render(request, 'petition/petition_detail.html', {'petition': petition, 'signatures': signatures})
