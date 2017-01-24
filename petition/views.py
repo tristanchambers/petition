7bb55dc9e0c8cf4d2134a07694c02d01c2f662d0
Tristan Chambers <tristan.chambers@gmail.com>
2017-01-23 23:32:47 -0500

    Add cookie to check if user already signed

--- petition/views.py
+++ petition/views.py
@@ -10,19 +10,22 @@ from .forms import SignatureForm
 
 def petition_list(request):
     petitions = Petition.objects.all()
-    return render(request, 'petition/petition_list.html', {'petitions': petitions})
+    return render(request, 'petition/petition_list.html', {'petitions': petitions, 'visited': visited})
 
 def petition_detail(request, primary_key):
     petition = get_object_or_404(Petition, pk=primary_key)
     signatures = Signature.objects.filter(petition=1)
-
     # If this is a form submission, process the request
+    # check if the user already signed the petition
+    signed = request.session.get('has_signed', False)
     if request.method == "POST":
-        signform_data = SignatureForm(request.POST)
-        if signform_data.is_valid():
-            signature = signform_data.save(commit=False)
-            signature.petition = petition
-            signature.save()
+        if not signed:
+            signform_data = SignatureForm(request.POST)
+            if signform_data.is_valid():
+                signature = signform_data.save(commit=False)
+                signature.petition = petition
+                signature.save()
+                request.session['has_signed'] = True
 
     # Either way render the petition details
     signform = SignatureForm()
@@ -33,5 +36,6 @@ def petition_detail(request, primary_key):
             'petition': petition,
             'signatures': signatures,
             'signform': signform,
+            'signed': signed,
         },
     )
