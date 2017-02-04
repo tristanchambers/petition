f39d9cd4422b4c649ccb646bcfef01da6b56f6c4
Tristan Chambers <tristan.chambers@gmail.com>
2017-02-04 08:19:44 -0500

    Add useful fields to signature listing in admin

--- petition/admin.py
+++ petition/admin.py
@@ -8,16 +8,17 @@ from .models import Signature
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-class SignatureInline(admin.TabularInline):
-    model = Signature
-
+# Set up import/export utility on signatures
 class SignatureResource(resources.ModelResource):
 
     class Meta:
         model = Signature
 
+# Set up custom Signature admin settings
 class SignatureIEAdmin(ImportExportModelAdmin):
     resource_class = SignatureResource
+    list_display = ('first_name', 'last_name','city', 'state', 'created_date')
 
+# Register admin forms
 admin.site.register(Signature, SignatureIEAdmin)
 admin.site.register(Petition)
