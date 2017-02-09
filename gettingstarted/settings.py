b7a68aba154ee1bd91dd05eac9414161ff699bf6
Tristan Chambers <tristan.chambers@gmail.com>
2017-02-08 21:20:05 -0500

    Set DEBUG as an env var

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+if os.environ.get('PETITION_DEBUG'):
+    DEBUG = os.environ.get('PETITION_DEBUG')
 
 # Application definition
 
