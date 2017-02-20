2d75c09506386e526c787fd95207468f51b946a7
Tristan Chambers <tristan.chambers@gmail.com>
2017-02-20 15:34:20 -0500

    SEO: generate sitemap.xml using django.contrib.sitemaps

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
-
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
 from django.contrib import admin
 admin.autodiscover()
+
+from petition.models import Petition
+info_dict = {
+    'queryset': Petition.objects.all(),
+}
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
@@ -12,4 +18,8 @@ urlpatterns = [
     url(r'', include('petition.urls')),
     url(r'', include('letsencrypt.urls')),
     url(r'', include('googlewebmaster.urls')),
+    # the sitemap
+    url(r'^sitemap\.xml$', sitemap,
+        {'sitemaps': {'blog': GenericSitemap(info_dict, priority=0.6)}},
+        name='django.contrib.sitemaps.views.sitemap'),
 ]
